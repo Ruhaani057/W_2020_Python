24758cc208453f0eb2130896cbaa7141e0c9cb76
Ruhaani057 <jessica2000black@gmail.com>
2020-12-29 19:57:42 +0530

    created Day 1 folder

---
+++ Day 1/basics.py
@@ -0,0 +1,166 @@
+#print("Hello World!")
+
+"""
+Variables and data types
+"""
+#a = 15 #int
+#print(a)
+#print(type(a))
+
+#b = 10.2 #int
+#print(b)
+#print(type(b))
+
+#c = True #float
+#print(c)
+#print(type(c))
+
+#d = "Today is a sunny day" #float
+#print(d)
+#print(type(d))
+
+"""
+Operators
+    Aritmetic
+    add +,
+    sub -,
+    mul *, 
+    div /, #float
+    mod %,
+    int division    //, #int
+    pow **
+
+    Logical
+    OR 
+    ANd 
+    NOT
+
+    Comparison Operators
+    >,<,==,>=,<=,!=
+
+"""
+
+#a = 20
+#b = 3
+#print("sum : " , a+b)
+#print("difference : ", a-b)
+#print("product : ", a*b)
+#print("quotient : ", a/b) 
+#print("remainder : ", a%b)
+#print("integer quotient : ", a//b)
+#print("power : ", a**b)
+
+#s = "Today"
+#print(s+s) # + : concatenate
+#print (5*(s)) #today is printed 5 times
+
+"""
+Logical Ooperators
+"""
+#print(True and False)
+
+"Q1. Odd-Even"
+# n = int(input("Enter a number :"))
+# if n%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+"Q2. Remainder by 3"
+# n = int(input("Enter a number :"))
+# if n%3==0:
+#     print("Zero")
+# elif n%3==1:
+#     print("One")
+# else:
+#     print("Two")
+
+"""Q3. WAP input a number a print:
+Fizz : multiple of 3
+Buzz : multiple of 5
+Fizz Buzz : multiple of 3 and 5 """
+# n = int(input("Enter a number :"))
+# if n%3==0 and n%5==0:
+#     print("Fizz Buzz")
+# elif n%3==0 :
+#     print("Fizz")
+# elif n%5==0:
+#     print("Buzz")
+
+# n = int(input("Enter a number :"))
+# if n%3==0 and n%5==0:
+#     print("Fizz Buzz")
+# elif n%3==0 :
+#     print("Fizz")
+# elif n%5==0:
+#     print("Buzz")
+
+"Q4. Calculator"
+
+# # c = int(input("CALCULATOR \n 1. Addition\n 2.Subtraction\n 3. Multiply\n 4.Divide\n 5.Remainder\n 6.Integer Division\n 7.Power\n 8.Square root\n Enter serial number to select what to do :"))
+# print("CALCULATOR \n 1. Addition \t: +\n 2. Subtraction \t: -\n 3. Multiply \t: *\n 4. Divide \t: /\n 5. Remainder \t: %\n 6. Integer Division \t: //\n 7. Power \t: **\n 8. Square root \t: ^\n")
+# c= input("Enter operator : ")
+
+# if c=='+':
+#     a = int(input("Enter first number :"))
+#     b = int(input("Enter second number :"))
+#     print("Sum : " , a+b)
+# elif c=='-':
+#     a = int(input("Enter first number :"))
+#     b = int(input("Enter second number :"))  
+#     print("Difference : ", a-b)
+# elif c=="*":
+#     a = int(input("Enter first number :"))
+#     b = int(input("Enter second number :"))    
+#     print("Product : ", a*b)
+# elif c=="/":
+#     a = int(input("Enter dividend number :"))
+#     b = int(input("Enter divisor number :")) 
+#     print("Quotient : ", a/b) 
+# elif c=="%":
+#     a = int(input("Enter dividend number :"))
+#     b = int(input("Enter divisor number :")) 
+#     print("Remainder : ", a%b)
+# elif c=="//":
+#     a = int(input("Enter dividend number :"))
+#     b = int(input("Enter divisor number :")) 
+#     print("Integer quotient : ", a//b)
+# elif c=="**":
+#     a = int(input("Enter base number :"))
+#     b = int(input("Enter power number :")) 
+#     print("Power : ", a**b)
+# elif c=="^":
+#     a = int(input("Enter number :"))
+#     print("Square root : ", a**0.5)
+# else:
+#     print("Wrong operator")
+
+"Q4. Leap Year"
+# y = int(input("Enter year : "))
+# if y%400==0:
+#     print(y," is a leap year!") 
+# else:
+#     if y%100==0 :
+#         print("Sorry, ", y," is not a leap year!")
+#     elif y%4==0:
+#         print(y," is a leap year!") 
+#     else:
+#         print("Sorry, ", y," is not a leap year!")
+
+"""
+Loops
+    For
+    While  
+"""
+# i = 0       #counter
+# while i<10: #condition
+#     print('Hello')
+#     i +=1 
+
+"Table"
+# n = int(input("Enter a number :"))
+# for i in range(1, 11, 1):
+#     print(n*i)
+ 
+
+
